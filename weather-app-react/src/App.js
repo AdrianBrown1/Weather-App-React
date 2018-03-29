85d5377c6f641d4a78c7cbac407899ae6ac2373d
Adrian Brown <adrian.angel.brown@gmail.com>
2018-03-29 17:20:49 -0400

    refactor: refactored App.js to contain custom components

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+import Router from './router/router';
 
-class App extends Component {
+const store = configureStore({});
+
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <Router/>
+      </Provider>
     );
   }
 }
-
-export default App;
