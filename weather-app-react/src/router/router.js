ded0823b9b1ed7415166045053b24e10569d10d3
Adrian Brown <Adrian.Angel.Brown@gmail.com>
2020-05-05 20:31:13 -0400

    refactor: revisions

--- weather-app-react/src/router/router.js
+++ weather-app-react/src/router/router.js
@@ -1,19 +1,15 @@
-import React, { Fragment } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from 'react-router-dom';
-import { home } from '../constants/routes';
-import Dashboard from '../containers/dash-board-container/dash-board-container';
-import Error from '../components/error/error';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { home } from "../constants/routes";
+import Dashboard from "../containers/dash-board-container/dash-board-container";
+import Error from "../components/error/error";
 export default () => (
   <Router>
     <Fragment>
       <Switch>
-        <Route exact path={home} component={Dashboard}/>
-        <Route component={Error}/>
+        <Route exact path={home} component={Dashboard} />
+        <Route component={Error} />
       </Switch>
     </Fragment>
   </Router>
-)
+);
